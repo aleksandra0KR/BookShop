f274f702d8eed717a1bbd770c3047cc798579a2b
aleksandra2022itmo <kriuchkova.alexa@yandex.ru>
2025-04-23 22:55:20 +0300

    Initial Commit

---
+++ BookShopTests/BookShopTests.swift
@@ -0,0 +1,17 @@
+//
+//  BookShopTests.swift
+//  BookShopTests
+//
+//  Created by Александра Крючкова on 23.04.2025.
+//
+
+import Testing
+@testable import BookShop
+
+struct BookShopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
